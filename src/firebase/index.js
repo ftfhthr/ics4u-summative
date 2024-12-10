52414991c1350997b966b546a659a70a2f8f42f0
email <toby.deng@student.tdsb.on.ca>
2024-12-10 11:31:12 -0500

    init firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyB90ml6js2W1uh7WN6LYmKZ2avCa_i4rqg",
+    authDomain: "summative-acf4b.firebaseapp.com",
+    projectId: "summative-acf4b",
+    storageBucket: "summative-acf4b.firebasestorage.app",
+    messagingSenderId: "1038627803565",
+    appId: "1:1038627803565:web:6764cbd4763fd07ff49124"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
